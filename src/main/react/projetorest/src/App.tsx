6b8f70fdd28079ab99437a2b64da4f2aee0c7649
mateus-cirino <mateuscirinosilva32@gmail.com>
2020-10-25 23:24:33 -0300

    routes

--- src/main/react/projetorest/src/App.tsx
+++ src/main/react/projetorest/src/App.tsx
@@ -1,12 +1,17 @@
 import React, {useEffect, useState} from 'react';
 import Login from "./visoes/paginas/usuario/login/Login";
 import {Usuario} from "./modelos/usuario";
+import {BrowserRouter, Switch, Route} from "react-router-dom";
 
 function App() {
   const [usuario, setUsuario] = useState<Usuario>();
   useEffect(() => console.log(usuario), [usuario]);
     return (
-      <Login setUsuario={setUsuario} />
+        <BrowserRouter>
+          <Switch>
+            <Route path="/" component={() => <Login setUsuario={setUsuario} />}/>
+          </Switch>
+        </BrowserRouter>
   );
 }
 
