49ce24d769677a0bd72eb6c4f8d9c35bbdd446e3
Mateus <mateuscirinosilva32@gmail.com>
2020-11-23 23:55:46 -0300

    commit

--- src/main/react/projetorest/src/visoes/paginas/evento/relatorios/RelatorioParticipacaoSintetico.tsx
+++ src/main/react/projetorest/src/visoes/paginas/evento/relatorios/RelatorioParticipacaoSintetico.tsx
@@ -1,7 +1,7 @@
-import {useEffect, useState} from "react";
+import {useEffect, useRef, useState} from "react";
 import {recuperarRelatorioParticipacaoSintetico} from "../../../../servicos/evento.servico";
 import {useToasts} from "react-toast-notifications";
-import {Label} from "reactstrap";
+import {Button, Label} from "reactstrap";
 import React from "react";
 import Select from "react-select";
 import {Evento} from "../../../../modelos/evento";
@@ -10,11 +10,13 @@ import {CLASS_NAME_EVENTO} from "../../../../utils/nomeClasseVO";
 import {customStyles} from "../../../../utils/tabelaUtils";
 import DataTable from "react-data-table-component";
 import moment from "moment";
+import ReactToPrint from "react-to-print";
 
 const RelatorioParticipacaoSintetico = () => {
     const [dadosRelatorios, setDadosRelatorios] = useState([]);
     const [eventosOptions, setEventosOptions] = useState([]);
     const [selectEventoValue, setSelectEventoValue] = useState(null);
+    const componentRef = useRef();
     const { addToast } = useToasts();
     useEffect(() => {
         const entidadeEvento = {
@@ -57,23 +59,31 @@ const RelatorioParticipacaoSintetico = () => {
     };
     return (
         <div className="container">
-            <div>
-                <Label for="evento">Eventos</Label>
-                <Select
-                    placeholder="selecione o evento para filtrar"
-                    name="evento"
-                    value={selectEventoValue}
-                    options={eventosOptions}
-                    onChange={handleChangeEvento}
-                />
+            <div className="d-flex justify-content-center align-items-end">
+                <div className="col-8">
+                    <Label for="evento">Eventos</Label>
+                    <Select
+                        placeholder="selecione o evento para filtrar"
+                        name="evento"
+                        value={selectEventoValue}
+                        options={eventosOptions}
+                        onChange={handleChangeEvento}
+                    />
+                </div>
+                <div className="col-2">
+                    <ReactToPrint
+                        trigger={() => <Button color="primary">Imprimir</Button>}
+                        content={() => componentRef.current}/>
+                </div>
             </div>
-            <div className="d-flex flex-column align-items-end">
+            <div className="d-flex flex-column align-items-end" ref={componentRef}>
                 <DataTable
                     title="Relatório de Participação Sintético"
                     columns={columns}
                     className="border rounded p-2"
                     data={dadosRelatorios}
                     striped={true}
+                    dense={true}
                     highlightOnHover={true}
                     customStyles={customStyles}
                 />
